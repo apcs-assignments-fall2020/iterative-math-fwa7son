7aae02ef17f8d2c34ac3d55eaa53c130901cd041
fwa7son <21fwatson@athenian.org>
2020-09-13 20:10:45 +0000

    added functionality to the functions

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -4,29 +4,44 @@ public class MyMain {
     
     // Calculates the square root iteratively, using the Babylonian method
     public static double babylonian(double x) {
-        // REPLACE WITH YOUR CODE HERE
-        return -1;
+        double a = x;
+        double b = 1;
+        while ((a-b) > 0.000001) {
+            a = (a + b) / 2;
+            b = x / a;
+        }
+        return a;
     }
 
     // Calculates the factorial of a number
     public static double factorial(int x) {
-        // REPLACE WITH YOUR CODE HERE
-        return -1;
+        int total = 1;
+        for (int i = x; i > 1; i--) {
+            total *= i;
+        }
+        return total;
     }
 
     // Calculates the value for the math constant e iteratively
     // goes until it calculates an answer less than 0.00000000001 
     // different from the value Math.E
     public static double calculateE() {
-        // REPLACE WITH YOUR CODE HERE
-        return -1;
+        double e = 1;
+        int variable = 1;
+        while ((Math.E - e) > 0.00001) {
+            e += 1/factorial(variable);
+            variable += 1;
+        }
+        return e;
     }
-    
     
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        // YOUR CODE HERE
+        System.out.println("Enter a number: ");
+        double x = scan.nextDouble();
+        System.out.println("The square root of your input is " + babylonian(x));
+        System.out.println("The value of e is roughly " + calculateE());
 
 
         
